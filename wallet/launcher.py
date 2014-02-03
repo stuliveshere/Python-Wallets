8a49f868d613ca4540cc0677eb128972c7438cf9
Stu Fletcher <mail@stuliveshere.com>
2014-02-03 13:00:05 +1000

    set up tkinter class template

--- wallet/launcher.py
+++ wallet/launcher.py
@@ -1,8 +1,20 @@
-import tkinter as tk
+import Tkinter as tk
+
+
+class gui(tk.Tk):
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        pass
 
 
 def main():
-    print "hello world"
-    
+    app = gui(None)
+    app.title('Python-Wallets')
+    app.mainloop()
+
 if __name__ == "__main__":
     main()
