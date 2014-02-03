91bf79d86a5570cc2c0cd39b89efda131a02e2cc
Stu Fletcher <mail@stuliveshere.com>
2014-02-03 15:21:28 +1000

    started new database command

--- wallet/launcher.py
+++ wallet/launcher.py
@@ -1,17 +1,31 @@
-import Tkinter as tk
-import ttk
+from Tkinter import *
+from ttk import *
+from tkFileDialog import *
+import sys
 
-class View(tk.Toplevel):
+
+class View(Toplevel):
 
     def __init__(self, parent):
-        tk.Toplevel.__init__(self, parent)
+        Toplevel.__init__(self, parent)
 
         self.title('Python-Wallets')
-        self.geometry("1280x768")
+        self.geometry("1280x1024")
+        self.frame = Frame(self, name='descrip')
+        self.frame.pack(side=TOP, fill=BOTH, expand=Y)
+        self._menu()
+        self._notebook()
 
-        self.menuBar = tk.Menu(self)
+    def _notebook(self):
+        notebook = Notebook(self.frame)
+        notebook.enable_traversal()  
+        notebook.pack(fill='both', expand=Y, padx=5, pady=5)
+        View_Config(notebook)
+
+    def _menu(self):
+        self.menuBar = Menu(self)
         self["menu"] = self.menuBar
-        self.fileMenu = tk.Menu(self.menuBar)
+        self.fileMenu = Menu(self.menuBar)
         self.menuBar.add_cascade(label="File", menu=self.fileMenu)
         self.fileMenu.add_command(label="New")
         self.fileMenu.add_command(label="Open...")
@@ -22,32 +36,45 @@ class View(tk.Toplevel):
         self.fileMenu.add_command(label="Save As...")
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="Quit")
-        
-        self.toolMenu = tk.Menu(self.menuBar)
+        self.toolMenu = Menu(self.menuBar)
         self.menuBar.add_cascade(label="Tools", menu=self.toolMenu)
         self.toolMenu.add_command(label="template")
-
-        self.helpMenu = tk.Menu(self.menuBar)
+        self.helpMenu = Menu(self.menuBar)
         self.menuBar.add_cascade(label="Help", menu=self.helpMenu)
         self.helpMenu.add_command(label="About")
-       
-        notebook = ttk.Notebook(self)
-        notebook.pack(fill='both', expand=1, padx=5, pady=5)
-        notebook.enable_traversal()
-
-#        self.params = View_Param(notebook)
-#        self.volumes = View_Volumes(notebook)
-#        self.nodes = View_Nodes(notebook)
 
 
-class controller(tk.Toplevel):
+class View_Config():
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = Frame(parent)
+        self.view = parent.add(self.frame, text='Configure')
+
+
+class controller(Toplevel):
     def __init__(self, root):
         self.parent = root
         self.view = View(root)
+        self.view.protocol('WM_DELETE_WINDOW', self.quit)
+        
+        self.view.fileMenu.entryconfig(1, command=self.new)
+        self.view.fileMenu.entryconfig(9, command=self.quit)
+
+    def quit(self):
+        sys.exit()
+        
+    def new(self):
+        options = {
+        'defaultextension':".h5",
+        'filetypes':[('HDF5',"*.h5")],
+        'initialdir':"../",
+        'title':"new file name",
+        }
+        self.db_handle = asksaveasfile(**options)
 
 
 def main():
-    root = tk.Tk()
+    root = Tk()
     root.withdraw()
     app = controller(root)
     root.mainloop()
