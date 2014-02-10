7ef0506e592462e9bae32ad48b08ffd6d66fa901
Stu Fletcher <mail@stuliveshere.com>
2014-02-10 11:48:32 +1000

    stash

--- wallet/launcher.py
+++ wallet/launcher.py
@@ -2,16 +2,20 @@ from Tkinter import *
 from tkFileDialog import *
 import sys
 from view import *
+
 #from model import *
+
 
 class Wizard(object):
     def __init__(self, parent):
         '''new project wizard controller'''
         self.parent = parent
         self.view = WizardView(self.parent)
-        #bindings
-        self.view.p1.next.config(command=lambda: self.view.notebook.select(1))
-
+#        #bindings
+#        self.view.p1.next.config(command=lambda: self.view.notebook.select(1))
+#        self.view.p2.next.config(command=lambda: self.view.notebook.select(2))
+#        self.view.p2.back.config(command=lambda: self.view.notebook.select(1))
+#        self.view.p3.back.config(command=lambda: self.view.notebook.select(2))
 
 class Gui(object):
     ''' main gui controller'''
@@ -29,6 +33,8 @@ class Gui(object):
     def new(self):
         '''new database wizard'''
         Wizard(self.parent)
+
+        
 
 
 def main():
